3239f565beca16204c140e97f83b9a33a360a4cd
AutoMagicNinja <AutoMagicNinja@users.noreply.github.com>
2023-01-10 21:26:14 -0800

    feat(day-04): add code for first star

---
+++ day-04/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"github.com/spf13/afero"
+	flag "github.com/spf13/pflag"
+)
+
+var inputFilename string
+
+func init() {
+	flag.StringVar(&inputFilename, "input", "", "Input filename")
+}
+
+func firstStar(lines []string) {
+	var answer int = 0
+	for lineNumber, eachLine := range lines {
+		pair, err := NewPair(eachLine)
+		if err != nil {
+			log.Printf("error on line %d: %s", lineNumber+1, err.Error())
+			continue
+		}
+		a, b := pair[0], pair[1]
+		if a.Contains(b) || b.Contains(a) {
+			// log.Printf("line contains: %s", eachLine)
+			answer += 1
+		}
+	}
+	log.Printf("total: %d", answer)
+}
+
+func main() {
+	var (
+		err     error
+		inputFs afero.Fs = afero.NewReadOnlyFs(afero.NewOsFs())
+		rawData []byte
+		lines   []string
+	)
+
+	flag.Parse()
+	log.Printf("input filename: %#v\n", inputFilename)
+
+	rawData, err = afero.ReadFile(inputFs, inputFilename)
+	if err != nil {
+		panic(err)
+	}
+
+	lines = strings.Split(strings.TrimSpace(string(rawData)), "\n")
+	firstStar(lines)
+}
---
+++ day-04/range.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type SectionRange struct {
+	Start int
+	End   int
+}
+
+func NewRange(section string) (*SectionRange, error) {
+	var (
+		err   error = nil
+		sr          = new(SectionRange)
+		parts       = strings.Split(strings.TrimSpace(section), "-")
+	)
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("incorrect number of parts for range")
+	}
+	sr.Start, err = strconv.Atoi(parts[0])
+	if err != nil {
+		return nil, fmt.Errorf("bad start of range: %s (%w)", parts[0], err)
+	}
+	sr.End, err = strconv.Atoi(parts[1])
+	if err != nil {
+		return nil, fmt.Errorf("bad end of range: %s (%w)", parts[1], err)
+	}
+	return sr, err
+}
+
+func NewPair(pairDefinition string) ([]*SectionRange, error) {
+	var (
+		err   error = nil
+		pair        = make([]*SectionRange, 2)
+		parts       = strings.Split(strings.TrimSpace(pairDefinition), ",")
+	)
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("incorrect number of parts for pair")
+	}
+	for k, v := range parts {
+		pair[k], err = NewRange(v)
+		if err != nil {
+			return nil, fmt.Errorf("bad definition for part %d of pair: %w", k+1, err)
+		}
+	}
+	return pair, err
+}
+
+func (sr SectionRange) Contains(osr *SectionRange) bool {
+	return (sr.Start <= osr.Start) && (sr.End >= osr.End)
+}

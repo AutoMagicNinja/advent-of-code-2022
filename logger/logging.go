70ebc0db3c719cda7bbe5ff2995236b7ff1b15ee
AutoMagicNinja <AutoMagicNinja@users.noreply.github.com>
2023-01-10 22:03:19 -0800

    feat(logger): add initial implementation of logger library

---
+++ logger/logging.go
@@ -0,0 +1,48 @@
+package logger
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	ZapLogEncoderConfig zapcore.EncoderConfig
+	ZapLogEncoderJSON   zapcore.Encoder
+	ZapLogEncoderHuman  zapcore.Encoder
+
+	Logger zap.SugaredLogger // Global logger
+)
+
+// Use init() function in order to guarantee the logger is created every* time
+func init() {
+
+	ZapLogEncoderConfig = zap.NewProductionEncoderConfig()
+	ZapLogEncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	ZapLogEncoderConfig.TimeKey = "@timestamp"
+	ZapLogEncoderConfig.CallerKey = "debug.caller"
+	ZapLogEncoderConfig.FunctionKey = "debug.function"
+	ZapLogEncoderConfig.MessageKey = "message"
+
+	ZapLogEncoderJSON = zapcore.NewJSONEncoder(ZapLogEncoderConfig)
+	ZapLogEncoderHuman = zapcore.NewConsoleEncoder(ZapLogEncoderConfig)
+
+	Logger = *zap.New(
+		zapcore.NewCore(ZapLogEncoderHuman, zapcore.AddSync(os.Stdout), zap.WarnLevel),
+	).Sugar()
+}
+
+func ResetLevel(logLevel zapcore.Level) zap.SugaredLogger {
+	Logger = *zap.New(
+		zapcore.NewCore(ZapLogEncoderHuman, zapcore.AddSync(os.Stdout), logLevel),
+	).Sugar()
+	return Logger
+}
+
+func ResetEncoder(encoder zapcore.Encoder) zap.SugaredLogger {
+	Logger = *zap.New(
+		zapcore.NewCore(ZapLogEncoderHuman, zapcore.AddSync(os.Stdout), Logger.Level),
+	).Sugar()
+	return Logger
+}
